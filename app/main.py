aa78be1932c5774bad7ebfc761dc80fd87ea381d
naruto <you@example.com>
2021-07-08 17:18:19 -0300

    added aiokafka connection and Dockerfile RUN pip

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
+import asyncio
+import uvicorn
+import json
+from aiokafka import AIOKafkaProducer
 from fastapi import FastAPI
 from schema.contact import Contact
 
 app = FastAPI()
+producer = None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def startup_event():
+    global producer
+    producer = AIOKafkaProducer(
+        bootstrap_servers=['172.30.0.3:9092'])
+    await producer.start()
 
+@app.on_event("shutdown")
+async def shutdown_event():
+    await producer.stop()
 
 @app.post("/")
 def create_contact(contact: Contact):
